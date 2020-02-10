d6c7b8f35e867b15aa91d2c8c27a38d5198e622d
Даниил Хозя <I.O.Stalina@hotmail.com>
2020-02-10 17:06:57 +0300

    Добавьте файлы проекта.

---
+++ Planirovshik-core 3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Planirovshik_core_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
